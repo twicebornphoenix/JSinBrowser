ff4fa6c576846df2f6570206fbfd60511acfdf9a
twiceborn.phoenix <twiceborn.phoenix@gmail.com>
2018-07-09 21:32:29 +0300

    dom theme tasks were done

---
+++ dom/shop/js/script.js
@@ -0,0 +1,11 @@
+const btns = Array.from(document.querySelectorAll('.add'));
+
+btns.forEach(btn => btn.addEventListener('click', addItem));
+
+function addItem() {
+	let itemsIn = document.getElementById('cart-count');
+	let totalPrice = document.getElementById('cart-total-price');
+
+	itemsIn.textContent = +itemsIn.textContent + 1;
+	totalPrice.textContent = +totalPrice.textContent + +this.dataset.price; 
+}
