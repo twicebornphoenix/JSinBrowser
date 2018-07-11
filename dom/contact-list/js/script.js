026cd4eecc8d499499ab162a3e337001eb8c5083
Twice Born Phoenix <twiceborn.phoenix@gmail.com>
2018-07-11 14:40:54 +0300

    ..il

---
+++ dom/contact-list/js/script.js
@@ -0,0 +1,8 @@
+const contactsData = JSON.parse(loadContacts());
+const contactsItem = document.querySelector('.contacts-list');
+
+contactsItem.innerHTML = '';
+
+for (let i = 0; i < contactsData.length; i++) {
+	contactsItem.innerHTML += `<li data-email="${contactsData[i].email}" data-phone="${contactsData[i].phone}"><strong>${contactsData[i].name}</strong></li>`;
+} 
