0fa9f6237dafe2e9480b0181bea8fd376847676d
twiceborn.phoenix <twiceborn.phoenix@gmail.com>
2018-07-17 21:30:27 +0300

    done

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const itemsList = document.querySelector('.items-list');
+let addBtns = Array.from(itemsList.querySelectorAll('.add-to-cart'));
+
+function collectAddBtns() {
+	addBtns = Array.from(itemsList.querySelectorAll('.add-to-cart'));
+	setListener();
+}
+
+function setListener() {
+	addBtns.forEach(btn => btn.addEventListener('click', prepareToAdd));
+}
+
+function prepareToAdd(e) {
+	e.preventDefault();
+	addToCart(e.target.dataset);
+}
+
+showMore.addEventListener('click', collectAddBtns);
+addBtns.forEach(btn => btn.addEventListener('click', prepareToAdd));
