34335224089ad317866f619aeca774c73031dbf5
twiceborn.phoenix <twiceborn.phoenix@gmai.com>
2018-07-25 23:17:02 +0300

    counter done

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const ws = new WebSocket('wss://neto-api.herokuapp.com/counter');
+const counter = document.querySelector('.counter');
+const errors = document.querySelector('.errors');
+
+ws.addEventListener('message', function(e) {
+	let connection = JSON.parse(e.data);
+	counter.innerHTML = connection.connections;
+	errors.innerHTML = connection.errors;
+})
+
+window.addEventListener('beforeunload', function(e) {
+	ws.close(1000, "Соединение будет закрыто");
+});
