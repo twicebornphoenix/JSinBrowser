284abd076da0791f759abbae81341d0bf6ce6806
twiceborn.phoenix <twiceborn.phoenix@gmail.com>
2018-08-02 05:47:53 +0300

    some commit

--- canvas/draw/js/draw.js
+++ canvas/draw/js/draw.js
@@ -1,1 +1,80 @@
 'use strict';
+
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+
+canvas.style.display = 'block';
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let isActive = false;
+
+function clearSize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function changeHSL(e) {
+    const startHSL = 0;
+    const endHSL = 359;
+    let curHSL = 0;
+
+    e.shiftKey ? curHSL-- : curHSL++;
+
+    if (curHSL > 359) {
+        curHSL = 0;
+    } else if (curHSL < 0) {
+        curHSL = 359;
+    }
+
+    return curHSL;
+}
+
+function changeSize() {
+    const minSize = 5;
+    const maxSize = 100;
+    let curSize = 0;
+
+    if (maxSize > curSize) {
+        curSize++;
+    } else {
+        curSize--;
+    }
+    if (curSize === 100) {
+        maxSize = 5;
+    }
+    if (curSize === 5) {
+        maxSize = 100;
+    }
+
+    return curSize;
+}
+
+function draw(e) {
+    if (isActive) {
+    	let x = e.clientX;
+    let y = e.clientY;
+    let curHSL = changeHSL(e);
+    let curSize = changeSize()
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+
+    ctx.beginPath();
+    ctx.lineTo(x, y);
+    ctx.strokeStyle = `hsl(${curHSL}, 100%, 50%)`;
+    ctx.lineWidth = curSize;
+    ctx.stroke();
+}
+}
+
+window.addEventListener('resize', clearSize);
+canvas.addEventListener('dblclick', clearSize);
+canvas.addEventListener('mousemove', draw)
+canvas.addEventListener('mousedown', () => {
+    isActive = true;
+});
+canvas.addEventListener('mouseup', () => {
+    isActive = false;
+})
--- dom-from-zero/comments/js/comments.js
+++ dom-from-zero/comments/js/comments.js
@@ -1,31 +1,89 @@
 'use strict';
 
 function showComments(list) {
-  const commentsContainer = document.querySelector('.comments');
-  const comments = list.map(createComment).join('');
-  commentsContainer.innerHTML += comments;
+    const commentsContainer = document.querySelector('.comments');
+    console.log(commentsContainer)
+    const comments = document.createDocumentFragment();
+
+    console.log(list, comments)
+    
+    list.forEach(c => {
+        comments.appendChild(createComment(c));
+    });
+
+    commentsContainer.appendChild(comments);
 }
+// function createComment(comment) {
+//   return `<div class="comment-wrap">
+//     <div class="photo" title="${comment.author.name}">
+//       <div class="avatar" style="background-image: url('${comment.author.pic}')"></div>
+//     </div>
+//     <div class="comment-block">
+//       <p class="comment-text">
+//         ${comment.text.split('\n').join('<br>')}
+//       </p>
+//       <div class="bottom-comment">
+//         <div class="comment-date">${new Date(comment.date).toLocaleString('ru-Ru')}</div>
+//         <ul class="comment-actions">
+//           <li class="complain">Пожаловаться</li>
+//           <li class="reply">Ответить</li>
+//         </ul>
+//       </div>
+//     </div>
+//   </div>`
+// }
 
 function createComment(comment) {
-  return `<div class="comment-wrap">
-    <div class="photo" title="${comment.author.name}">
-      <div class="avatar" style="background-image: url('${comment.author.pic}')"></div>
-    </div>
-    <div class="comment-block">
-      <p class="comment-text">
-        ${comment.text.split('\n').join('<br>')}
-      </p>
-      <div class="bottom-comment">
-        <div class="comment-date">${new Date(comment.date).toLocaleString('ru-Ru')}</div>
-        <ul class="comment-actions">
-          <li class="complain">Пожаловаться</li>
-          <li class="reply">Ответить</li>
-        </ul>
-      </div>
-    </div>
-  </div>`
+    const element = document.createElement('div');
+    const photoBlock = document.createElement('div');
+    const photoAvatar = document.createElement('div');
+    const commentBlock = document.createElement('div');
+    const commentText = document.createElement('p');
+    const commentBottom = document.createElement('div');
+    const commentDate = document.createElement('div');
+    const actionList = document.createElement('ul');
+    const complainItem = document.createElement('li');
+    const replyItem = document.createElement('li');
+
+    element.className = 'comment-wrap';
+    photoBlock.className = 'photo';
+    photoBlock.title = comment.author.name;
+    photoAvatar.className = 'avatar';
+    photoAvatar.style.backgroundImage = `url(${comment.author.pic})`;
+    commentBlock.className = 'comment-block';
+    commentText.className = 'comment-text';
+    commentBottom.className = 'bottom-comment';
+    commentDate.className = 'comment-date';
+    commentDate.textContent = new Date(comment.date).toLocaleString('ru-Ru');
+    actionList.className = 'comment-actions';
+    complainItem.className = 'complain';
+    replyItem.className = 'reply';
+    complainItem.textContent = 'Пожаловаться';
+    replyItem.textContent = 'Ответить';
+
+    commentText.innerHTML = comment.text.split('\n').join("<br />");
+      console.log(commentText, commentText.innerHTML)
+
+    element.appendChild(photoBlock);
+    element.appendChild(commentBlock);
+
+    commentBlock.appendChild(commentText);
+    commentBlock.appendChild(commentBottom);
+
+    // commentText.appendChild(getMsg(comment));
+
+    photoBlock.appendChild(photoAvatar);
+
+    commentBottom.appendChild(commentDate);
+    commentBottom.appendChild(actionList);
+
+    actionList.appendChild(complainItem);
+    actionList.appendChild(replyItem);
+
+      console.log(element)
+    return element;
 }
 
 fetch('https://neto-api.herokuapp.com/comments')
-  .then(res => res.json())
-  .then(showComments);
+    .then(res => res.json())
+    .then(showComments);
