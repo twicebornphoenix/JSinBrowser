25f440f251f024c675d426eccd5042942733f2c9
Twice Born Phoenix <twiceborn.phoenix@gmail.com>
2018-08-02 16:01:39 +0300

    some commit

--- dom-from-zero/plane/js/scheme.js
+++ dom-from-zero/plane/js/scheme.js
@@ -1,1 +1,33 @@
 'use strict'
+
+const form = document.querySelector('.form-inline');
+const shemeArea = document.querySelector('#seatMapDiv');
+const showSheme = form.querySelector('#btnSeatMap');
+const setFull = form.querySelector('#btnSetFull');
+const setEmpty = form.querySelector('#btnSetEmpty');
+const mapTitle = document.querySelector('#seatMapTitle');
+const total = document.querySelector('#totalPax');
+const adult = document.querySelector('#totalAdult');
+const half = document.querySelector('#totalHalf');
+const select = Array.from(form.elements).find((el, index) => form[index].tagName === 'SELECT' ? el : false );
+
+setFull.disabled = true;
+setEmpty.disabled = true;
+let plain;
+
+function getData(e) {
+	return fetch(`https://neto-api.herokuapp.com/plane/${e.target.value}`)
+		.then(res => res.json())
+		.then(json => plain = json);
+}
+
+function showData(e, obj) {
+	e.preventDefault();
+	
+	obj = plain;
+	mapTitle.textContent = `${obj.title} (${obj.passengers} пассажиров)`;
+	
+}
+
+form.addEventListener('change', getData);
+showSheme.addEventListener('click', showData);
