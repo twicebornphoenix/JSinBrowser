4a535e065b70b14ddf9f2ceb286fa73086c10c15
twiceborn.phoenix <twiceborn.phoenix@gmail.com>
2018-08-03 03:43:21 +0300

    some commit

--- dom-from-zero/plane/js/scheme.js
+++ dom-from-zero/plane/js/scheme.js
@@ -6,28 +6,122 @@ const showSheme = form.querySelector('#btnSeatMap');
 const setFull = form.querySelector('#btnSetFull');
 const setEmpty = form.querySelector('#btnSetEmpty');
 const mapTitle = document.querySelector('#seatMapTitle');
+const mapDiv = document.querySelector('#seatMapDiv');
 const total = document.querySelector('#totalPax');
 const adult = document.querySelector('#totalAdult');
 const half = document.querySelector('#totalHalf');
-const select = Array.from(form.elements).find((el, index) => form[index].tagName === 'SELECT' ? el : false );
+const select = form.querySelector('select');
 
 setFull.disabled = true;
 setEmpty.disabled = true;
-let plain;
+let data = getData(); 
 
 function getData(e) {
-	return fetch(`https://neto-api.herokuapp.com/plane/${e.target.value}`)
+	const id = e ? e.target.value : 'a319'; 
+	fetch(`https://neto-api.herokuapp.com/plane/${id}`)
 		.then(res => res.json())
-		.then(json => plain = json);
+		.then(json => data = json);
 }
 
-function showData(e, obj) {
+function setData(e) {
 	e.preventDefault();
 	
-	obj = plain;
-	mapTitle.textContent = `${obj.title} (${obj.passengers} пассажиров)`;
+	// mapTitle.textContent = `${data.title} (${data.passengers} пассажиров)`;
 	
+
+	let x = createRowTempl(data)
+	console.log(x, data)
+}
+
+function createRowTempl(data) {
+	return {
+		tag: 'div',
+		cls: ['row', 'seating-row', 'text-center'],
+		block: [
+		{
+			tag: 'div',
+			cls: ['col-xs-1', 'row-number'],
+			block: {
+				tag: 'h2',
+				cls: '',
+				block: 'Номер ряда'
+			}
+		},
+		{
+			tag: 'div',
+			cls: 'col-xs-5',
+			block: [
+			{
+				tag: 'div',
+				cls: ['col-xs-4', 'seat'],
+				block: {
+					tag: 'span',
+					cls: 'seat-label',
+					block: 'Буква места'
+				}
+			},
+			{
+				tag: 'div',
+				cls: ['col-xs-4', 'seat'],
+				block: {
+					tag: 'span',
+					cls: 'seat-label',
+					block: 'Буква места'
+				}
+			},
+			{
+				tag: 'div',
+				cls: ['col-xs-4', 'seat'],
+				block: {
+					tag: 'span',
+					cls: 'seat-label',
+					block: 'Буква места'
+				}
+			}]
+		},
+		{
+			tag: 'div',
+			cls: 'col-xs-5',
+			block: [
+			{
+				tag: 'div',
+				cls: ['col-xs-4', 'seat'],
+				block: {
+					tag: 'span',
+					cls: 'seat-label',
+					block: 'Буква места'
+				}
+			},
+			{
+				tag: 'div',
+				cls: ['col-xs-4', 'seat'],
+				block: {
+					tag: 'span',
+					cls: 'seat-label',
+					block: 'Буква места'
+				}
+			},
+			{
+				tag: 'div',
+				cls: ['col-xs-4', 'seat'],
+				block: {
+					tag: 'span',
+					cls: 'seat-label',
+					block: 'Буква места'
+				}
+			}]
+		}]
+	}
 }
 
 form.addEventListener('change', getData);
-showSheme.addEventListener('click', showData);
+showSheme.addEventListener('click', setData);
+
+
+
+
+
+
+
+
+
