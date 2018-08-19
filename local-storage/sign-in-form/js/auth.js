22047494a8142b533cf6480a9b8ec14026d4d1aa
twiceborn.phoenix <twiceborn.phoenix@gmail.com>
2018-08-19 15:47:13 +0300

    dont work sign-up

--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -1,48 +1,47 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function() {
-
 	const signIn = document.querySelector('.sign-in-htm');
 	const signUp = document.querySelector('.sign-up-htm');
+	const outputSUp = signUp.querySelector('.error-message');
+	const outputSIn = signIn.querySelector('.error-message');
 
 	signIn.addEventListener('submit', function(e) {
-		let formData = new FormData(signIn);
-		let xhr = new XMLHttpRequest();
+		e.preventDefault();
+		const xhr = new XMLHttpRequest();
 
-		xhr.open('POST', 'https://neto-api.herokuapp.com/signin', true);
+		xhr.open('POST', 'https://neto-api.herokuapp.com/signin');
+		const formData = new FormData(signIn);
 		xhr.send(formData);
 
 		xhr.addEventListener('load', function() {
 			try {
 				var result = JSON.parse(xhr.responseText);
 				if (result.error) throw e;
-				signIn.querySelector('.error-message').textContent = `Пользователь ${result.name} успешно вошёл`;
+				outputSIn.textContent = `Пользователь ${result.name} успешно вошёл`;
 			} catch(e) {
-				signIn.querySelector('.error-message').textContent = result.message;
+				outputSIn.textContent = result.message;
 			}
 		});
 
-		e.preventDefault();
 	});
 
-	signUp.addEventListener('submit', function(e) {
-		let formData = new FormData(signUp);
-		let xhr = new XMLHttpRequest();
-
-		xhr.open('POST', 'https://neto-api.herokuapp.com/signup', true);
-		xhr.send(formData);
-
-		xhr.addEventListener('load', function() {
-			try {
-				var result = JSON.parse(xhr.responseText);
-				if (result.error) throw e;
-				signUp.querySelector('.error-message').textContent = `Пользователь ${result.name} успешно зарегистрирован`;
-			} catch(e) {
-				signUp.querySelector('.error-message').textContent = result.message;
-			}
-		});
-
+	signUp.addEventListener('submit', e => {
 		e.preventDefault();
+		let dataForm = {};
+
+		const formData = new FormData(signUp);
+		for (const [k, v] of formData) {
+			dataForm[k] = v;
+		}
+
+		fetch('https://neto-api.herokuapp.com/signup', {
+			body: JSON.stringify(dataForm),
+			method: 'POST'
+		})
+		.then(res => res.json())
+		.then(data => {
+			data.error ? outputSUp.textContent = data.message : outputSUp.textContent = 'Пользователь зарегистрирован';
+		})
 	});
 });
-
