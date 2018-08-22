6a524aa052d59b9ac5b08b191ae3006082ab1670
Twice Born Phoenix <twiceborn.phoenix@gmail.com>
2018-08-22 17:32:53 +0300

    added few comments

--- cors/profile/js/profile.js
+++ cors/profile/js/profile.js
@@ -3,34 +3,37 @@
 const url = 'https://neto-api.herokuapp.com/profile/me';
 
 function getData(url) {
-	const funcName = randomName();
-	return new Promise((success, fail) => {
-		window[funcName] = success;
-
-		const script = document.createElement('script');
-		script.src = `${url}?callback=${funcName}`;
-		document.body.appendChild(script);
-	});
+    //генерируем рандомное имя для функции
+    const funcName = randomName();
+    return new Promise((success, fail) => {
+        //привязываем выполнение функции при успешном исходе
+        window[funcName] = success;
+        //создаём элемент script, присваиваем ему ссылку с jsonp и помещаем в body
+        const script = document.createElement('script');
+        script.src = `${url}?callback=${funcName}`;
+        document.body.appendChild(script);
+    });
 }
 
 function parseData(data) {
-	const mainInfo = document.querySelector('.firstinfo');
-	for (const chunck in data) {
-		if (chunck === 'id') {
-			getData(`https://neto-api.herokuapp.com/profile/${data[chunck]}/technologies`)
-					.then(parseData);
-				} else {
-		const el = document.querySelector(`[data-${chunck}]`);
-		console.log(data[chunck])
-		// if (el.tagName !== 'IMG') el.textContent = data[chunck];
-	}
-	}
+    const mainInfo = document.querySelector('.firstinfo');
+    //обрабатываем полученные данные
+    for (const chunck in data) {
+        if (chunck === 'id') {
+            getData(`https://neto-api.herokuapp.com/profile/${data[chunck]}/technologies`)
+                .then(parseData);
+        } else {
+            const el = document.querySelector(`[data-${chunck}]`);
+            console.log(data[chunck])
+            // if (el.tagName !== 'IMG') el.textContent = data[chunck];
+        }
+    }
 }
 
 function randomName() {
-	const rawName = (Math.random() * 100).toString(36);
-	return isNaN(rawName.slice(4)[0]) ? rawName.slice(4) : randomName();
+    const rawName = (Math.random() * 100).toString(36);
+    return isNaN(rawName.slice(4)[0]) ? rawName.slice(4) : randomName();
 }
 
 getData(url)
-	.then(parseData);
+    .then(parseData);
