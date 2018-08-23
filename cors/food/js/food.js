91f95a95723ddddde9a0c23e6fa8eefad97a13d6
Twice Born Phoenix <twiceborn.phoenix@gmail.com>
2018-08-23 15:39:12 +0300

    ...

--- cors/food/js/food.js
+++ cors/food/js/food.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const urlRecipe = 'https://neto-api.herokuapp.com/food/42';
+const urlRating = 'https://neto-api.herokuapp.com/food/42/rating';
+const urlConsumers = 'https://neto-api.herokuapp.com/food/42/consumers'
+
+function getData(url) {
+    const funcName = randomName();
+
+    return new Promise((success, fail) => {
+        window[funcName] = success;
+
+        const script = document.createElement('script');
+        script.src = `${url}?callback=${funcName}`;
+        document.body.appendChild(script);
+    });
+}
+
+function parseData(data) {
+	for (const chunk in data) {
+		const el = document.querySelector(`[data-${chunk}]`);
+		console.log(el);
+	}
+}
+
+function randomName() {
+    const rawName = (Math.random() * 100).toString(36);
+    return isNaN(rawName.slice(4)[0]) ? rawName.slice(4) : randomName();
+}
+
+getData(urlRecipe)
+    .then(parseData);
+getData(urlRating)
+		.then(parseData);
+getData(urlConsumers)
+		.then(parseData);
--- cors/profile/js/profile.js
+++ cors/profile/js/profile.js
@@ -3,13 +3,11 @@
 const url = 'https://neto-api.herokuapp.com/profile/me';
 
 function getData(url) {
-    //генерируем рандомное имя для функции
     const funcName = randomName();
+
     return new Promise((success, fail) => {
-        //привязываем выполнение функции при успешном исходе
         window[funcName] = success;
-        //создаём элемент script, присваиваем ему ссылку с jsonp и помещаем в body
-        // const script = document.scripts[0].cloneNode();
+
         const script = document.createElement('script');
         script.src = `${url}?callback=${funcName}`;
         document.body.appendChild(script);
@@ -20,7 +18,7 @@ function parseData(data) {
     const mainInfo = document.querySelector('.firstinfo');
     const badges = document.querySelector('.badgescard');
     const content = document.querySelector('.content');
-    //обрабатываем полученные данные
+
     for (const chunck in data) {
         if (chunck === 'id') {
             getData(`https://neto-api.herokuapp.com/profile/${data[chunck]}/technologies`)
@@ -28,19 +26,19 @@ function parseData(data) {
         } else {
             const el = document.querySelector(`[data-${chunck}]`);
             if (el !== null) {
-            	if (el.tagName === 'IMG') {
-            		el.src = data[chunck]
-            	} else {
-            		el.textContent = data[chunck]
-            	}
+                if (el.tagName === 'IMG') {
+                    el.src = data[chunck]
+                } else {
+                    el.textContent = data[chunck]
+                }
             } else {
-            		const tech = document.createElement('span');
-            		tech.classList.add('devicons', `devicons-${data[chunck]}`);
-            		badges.appendChild(tech);
+                const tech = document.createElement('span');
+                tech.classList.add('devicons', `devicons-${data[chunck]}`);
+                badges.appendChild(tech);
             }
         }
     }
-   content.style.display = '';
+    content.style.display = '';
 }
 
 function randomName() {
