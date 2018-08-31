6e151fc44080199ea0b7094c5c049657a381441d
Twice Born Phoenix <twiceborn.phoenix@gmail.com>
2018-08-31 14:05:06 +0300

    added animation

--- canvas/background/js/background.js
+++ canvas/background/js/background.js
@@ -1,130 +1,159 @@
-'use strict';
+'use strict'; 
 
-class AnimatedFigure {
-    constructor(ops, w, h) {
-        this.width = w;
-        this.height = h;
-        const options = ops;
-    }
+class AnimatedFigure { 
+    constructor(ops, w, h) { 
+        this.width = w; 
+        this.height = h; 
+        const options = ops; 
+    } 
 
-    static getRandomValue(max, min) {
-        return Math.random() * (max - min + 1) + min;
-    }
+    static getRandomValue(max, min) { 
+        return Math.random() * (max - min) + min; 
+    } 
 
-    static getFigureAmount() {
-        let amount = this.getRandomValue(options.maxAmount, options.minAmount);
-        const delta = amount % options.types.length;
-        if (delta !== 0) amount -= delta;
+    static getFigureAmount() { 
+        let amount = this.getRandomValue(options.maxAmount, options.minAmount); 
+        const delta = amount % options.types.length; 
+        if (delta !== 0) amount -= delta; 
 
-        return amount;
-    }
+        return amount; 
+    } 
 
-    static createFigures(classes, amount) {
-        const classesAmount = classes.length;
-        const figures = classes.map(cls => []);
+    static createFigures(classes, amount) { 
+        const classesAmount = classes.length; 
+        const figures = classes.map(cls => []); 
 
-        for (let i = 1; i <= amount; i++) {
-        	let index = i;
-        	
-            if (index < (amount / classesAmount)) {
-                index = 0;
-            } else if (index === (amount / classesAmount)) {
-            	console.log('hi')
-            	index = 0;
-            } else {
-                index = Math.ceil(index / (amount / classesAmount)) -1;
+        for (let i = 1; i <= amount; i++) { 
+            let index = i; 
+
+            if (index < (amount / classesAmount)) { 
+                index = 0; 
+            } else if (index === (amount / classesAmount)) { 
+                index = 0; 
+            } else { 
+                index = Math.ceil(index / (amount / classesAmount)) -1; 
+            } 
+
+            const x = this.getRandomValue(root.clientWidth, 0); 
+            const y = this.getRandomValue(root.clientHeight, 0); 
+            const size = this.getRandomValue(options.maxSize, options.minSize); 
+            const timeFunc = options.timeFuncs[Math.round(this.getRandomValue(options.timeFuncs.length - 1, 0))]; 
+
+            const figure = new classes[index](x, y, size, timeFunc); 
+            figures[index].push(figure); 
+        } 
+        return figures; 
+    } 
+
+    static startAnimation(ctxt, figures) {
+        const start = performance.now();
+        const duration = 1000 / options.fps;
+
+        requestAnimationFrame(function animate(time) {
+            let collapsed = time - start;
+
+            if (collapsed > duration) {
+                collapsed = 0;
+                ctxt.clearRect(0, 0, ctxt.canvas.width, ctxt.canvas.height);
+                figures.forEach(cls => {
+                    cls.forEach(f => {
+                        f.draw(ctxt);
+                    })
+                })
             }
-            const x = this.getRandomValue(root.clientWidth, 0);
-            const y = this.getRandomValue(root.clientHeight, 0);
-            const size = this.getRandomValue(options.maxSize, options.minSize);
-            const figure = new classes[index](x, y, size);
-
-            figures[index].push(figure)
-        }
-
-        return figures;
+            requestAnimationFrame(animate);   
+        })
     }
 
-    init() {
-        const canvas = document.querySelector('#wall');
-        const context = canvas.getContext('2d');
+    init() { 
+        const canvas = document.querySelector('#wall'); 
+        const context = canvas.getContext('2d'); 
 
-        const amount = AnimatedFigure.getFigureAmount();
-        const classes = options.types;
-        const figures = AnimatedFigure.createFigures(classes, amount);
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight; 
 
-        canvas.width = this.width;
-        canvas.height = this.height;
+        const classes = options.types; 
+        const amount = AnimatedFigure.getFigureAmount(); 
+        const figures = AnimatedFigure.createFigures(classes, amount); 
 
-        context.strokeStyle = options.figuresColor;
+        context.strokeStyle = options.figuresColor; 
+
+        AnimatedFigure.startAnimation(context, figures);
+    } 
+} 
+
+class Figure { 
+    constructor(x, y, z, f) { 
+        this.x = x;  
+        this.y = y; 
+        this.z = z; 
+        this.f = f; 
+        this.lineW = z * 5; 
+        this.baseCords = [this.x, this.y]; 
+    } 
+} 
+
+class Circle extends Figure { 
+    constructor(x, y, z, f) { 
+        super(x, y, z, f); 
+        this.radius = z * 12; 
+    } 
+
+    draw(ctxt) {
+        const figCords = this.f(this.baseCords[0], this.baseCords[1], Date.now());
         
-        figures.forEach(cls => {
-        	cls.forEach(f => f.draw(context));
-        });	
-    }
-}
+        ctxt.beginPath(); 
+        ctxt.arc(figCords.x, figCords.y, this.radius, 0, 2 * Math.PI); 
+        ctxt.lineWidth = this.lineW; 
+        ctxt.stroke(); 
+    } 
+} 
 
-class Figure {
-    constructor(x, y, sz) {
-        this.x = x;
-        this.y = y;
-        this.size = sz;
-        this.stroke = sz * 5;
-    }
-}
+class Cross extends Figure { 
+    constructor(x, y, z, f) { 
+        super(x, y, z, f); 
+        this.side = z * 20; 
+        this.xC = this.side / 2 + x; 
+        this.yC = this.side / 2 + y; 
+    } 
 
-class Circle extends Figure {
-    constructor(x, y, sz, stroke) {
-        super(x, y);
-        this.radius = sz * 12;
-    }
+    draw(ctxt) { 
+        const figCords = this.f(this.baseCords[0], this.baseCords[1], Date.now());
 
-    draw(ctxt) {
-        ctxt.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-        ctxt.stroke();
-    }
-}
-
-class Cross extends Figure {
-    constructor(x, y, sz) {
-        super(x, y);
-        this.size = sz * 20;
-        this.xC = this.size / 2 + x;
-        this.yC = this.size / 2 + y;
-    }
-
-    draw(ctxt) {
+        ctxt.beginPath(); 
         ctxt.moveTo(this.x, this.y);
-        ctxt.lineTo(this.x + this.size, this.y + this.size);
-        ctxt.moveTo(this.x + this.size, this.y);
-        ctxt.lineTo(this.x, this.y + this.size)
-        ctxt.stroke();
-    }
-}
+        ctxt.lineTo(this.x + this.side, this.y + this.side); 
+        ctxt.moveTo(this.x + this.side, this.y); 
+        ctxt.lineTo(this.x, this.y + this.side);
+        ctxt.lineWidth = this.lineW; 
+        ctxt.stroke(); 
+    } 
+} 
 
-const options = {
-    types: [Circle, Cross],
-    maxSize: 0.6,
+const options = { 
+    types: [Circle, Cross], 
+    maxSize: 0.6, 
     minSize: 0.1,
-    maxAmount: 200,
-    minAmount: 50,
-    figuresColor: '#ffffff',
-    timeFuncs: [
-        function nextPoint(x, y, time) {
-            return {
-                x: x + Math.sin((50 + x + (time / 10)) / 100) * 3,
-                y: y + Math.sin((45 + x + (time / 10)) / 100) * 4
-            };
-        },
-        function nextPoint(x, y, time) {
-            return {
-                x: x + Math.sin((x + (time / 10)) / 100) * 5,
-                y: y + Math.sin((10 + x + (time / 10)) / 100) * 2
-            }
-        }
-    ]
-}
+    maxAmount: 200, 
+    minAmount: 50, 
+    fps: 20, 
+    figuresColor: '#ffffff', 
+    timeFuncs: [ 
+        function nextPoint(x, y, time) { 
+            return { 
+                x: x + Math.sin((50 + x + (time / 10)) / 100) * 3, 
+                y: y + Math.sin((45 + x + (time / 10)) / 100) * 4 
+            }; 
+        }, 
+        function nextPoint(x, y, time) { 
+            return { 
+                x: x + Math.sin((x + (time / 10)) / 100) * 5, 
+                y: y + Math.sin((10 + x + (time / 10)) / 100) * 2 
+            } 
+        } 
+    ] 
+} 
 
-const root = document.documentElement;
-const animBg = new AnimatedFigure(options, root.clientWidth, root.clientHeight);
-animBg.init();
+const root = document.documentElement; 
+const animBg = new AnimatedFigure(options, root.clientWidth, root.clientHeight); 
+animBg.init(); 
