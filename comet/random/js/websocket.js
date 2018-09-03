20f2ba5a19e41dcc5f13c9d8a2e90c9043663b3b
Twice Born Phoenix <twiceborn.phoenix@gmail.com>
2018-09-03 10:07:31 +0300

    done

--- comet/random/js/websocket.js
+++ comet/random/js/websocket.js
@@ -1,2 +1,41 @@
 'use strict';
 
+const wsNumbers = document.querySelector('.websocket').children;
+
+// const ws = new WebSocket('wss://neto-api.herokuapp.com/comet/websocket');
+// ws.addEventListener('message', e => {
+// 	Array.from(wsNumbers).forEach(number => {
+// 		if (e.data === number.textContent) {
+// 			console.log(e.data)
+// 			number.classList.add('flip-it');
+// 			setTimeout(function() {number.classList.remove('flip-it')}, 3000);
+// 		}
+// 	})
+// });
+
+class Wsocket {
+	constructor(url, t) {
+		this.ws = new WebSocket(url);
+		this.t = t || 2000;
+	}
+	showNumber(e) {
+		Array.from(wsNumbers)
+			.forEach(number => {
+			if (e.data === number.textContent) {
+				number.classList.add('flip-it');
+				self.hideNumber(number);
+			}
+		})
+	}
+	hideNumber(number) {
+		setTimeout(function() {
+			number.classList.remove('flip-it')
+		}, this.t);
+	}
+	adListener(event) {
+		this.ws.addEventListener(event, this.showNumber)
+	}
+}
+
+const ws = new Wsocket('wss://neto-api.herokuapp.com/comet/websocket');
+ws.adListener('message')
